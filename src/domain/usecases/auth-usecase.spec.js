35ee6d82b32bbdd970309790befac0fe76a26e75
Rafael Silverio <fael_st@hotmail.com>
2022-10-31 18:41:01 -0300

    ensure AuthUseCase return null if loadUserByEmailRepository returns null

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -22,7 +22,11 @@ class AuthUseCase {
       throw new InvalidParamError('loadUserByEmailRepository')
     }
 
-    await this.loadUserByEmailRepository.load(email)
+    const user = await this.loadUserByEmailRepository.load(email)
+
+    if (!user) {
+      return null
+    }
   }
 }
 
@@ -73,5 +77,11 @@ describe('Auth Usecase', () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('valid@mail.com', 'any_password')
     expect(promise).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+  })
+
+  test('Should return null if LoadUserByEmailRepository returns null', async () => {
+    const { sut } = makeSut()
+    const acessToken = await sut.auth('invalid_email@mail.com', 'invalid_password')
+    expect(acessToken).toBeNull()
   })
 })
