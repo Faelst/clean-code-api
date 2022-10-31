6a3c53fefe41829ff08efb6fb272f2da35181d1f
Rafael Silverio <fael_st@hotmail.com>
2022-10-31 18:23:03 -0300

    move sut creation a helper method

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -17,30 +17,39 @@ class AuthUseCase {
   }
 }
 
+const makeSut = () => {
+  class LoadUserByEmailSpy {
+    async load (email) {
+      this.email = email
+    }
+  }
+
+  const loadUserByEmailSpy = new LoadUserByEmailSpy()
+  const sut = new AuthUseCase(loadUserByEmailSpy)
+
+  return {
+    sut, loadUserByEmailSpy
+  }
+}
+
 describe('Auth Usecase', () => {
   test('Should retrun null if no email provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
+
     const promise = sut.auth(null, 'any_password')
     expect(promise).rejects.toThrow(new MissingParamsError('email'))
   })
 
   test('Should retrun null if no password provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
+
     const promise = sut.auth('any_email@mail.com')
     expect(promise).rejects.toThrow(new MissingParamsError('password'))
   })
 
   test('Should call LoadUserByEmail with correct email', async () => {
     const expectEmail = 'valid@mail.com'
-
-    class LoadUserByEmailSpy {
-      async load (email) {
-        this.email = email
-      }
-    }
-
-    const loadUserByEmailSpy = new LoadUserByEmailSpy()
-    const sut = new AuthUseCase(loadUserByEmailSpy)
+    const { sut, loadUserByEmailSpy } = makeSut()
     sut.auth(expectEmail, 'any password')
     expect(loadUserByEmailSpy.email).toEqual(expectEmail)
   })
