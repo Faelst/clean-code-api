4802d3c9260570ce6455fef8c0fc4f9f63cc0b1d
Rafael Silverio <fael_st@hotmail.com>
2022-11-08 15:25:52 -0300

    create makeSut helper of TokenGenerator

--- src/utils/token-generator.spec.js
+++ src/utils/token-generator.spec.js
@@ -6,16 +6,24 @@ class TokenGenerator {
   }
 }
 
+const makeSut = () => {
+  const sut = new TokenGenerator()
+
+  return {
+    sut
+  }
+}
+
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
     jwt.token = null
-    const sut = new TokenGenerator()
+    const { sut } = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(null)
   })
 
-  test('Should return null if JWT returns token', async () => {
-    const sut = new TokenGenerator()
+  test('Should return a token if JWT returns token', async () => {
+    const { sut } = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
   })
