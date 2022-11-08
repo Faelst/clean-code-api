32425f817150d59b1eb7833b4524e2bff7aa6a1e
Rafael Silverio <fael_st@hotmail.com>
2022-11-08 02:29:16 -0300

    ensure Encrypter call bcrypt with correct values

--- src/utils/encrypter.spec.js
+++ src/utils/encrypter.spec.js
@@ -1,14 +1,22 @@
 const bcrypt = require('bcrypt')
 
-class Encrypter {
-  async compare (value, hash) {
-    return bcrypt.compare(value, hash)
+const makeSut = () => {
+  class Encrypter {
+    async compare (value, hash) {
+      return bcrypt.compare(value, hash)
+    }
+  }
+
+  const encrypter = new Encrypter()
+
+  return {
+    sut: encrypter
   }
 }
 
 describe('Encrypter', () => {
   test('Should return if bcrypt returns true', async () => {
-    const sut = new Encrypter()
+    const { sut } = makeSut()
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(true)
   })
@@ -16,8 +24,15 @@ describe('Encrypter', () => {
   test('Should return if bcrypt returns false', async () => {
     bcrypt.isValid = false
 
-    const sut = new Encrypter()
+    const { sut } = makeSut()
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(false)
+  })
+
+  test('Should call bcrypt with correct values', async () => {
+    const { sut } = makeSut()
+    await sut.compare('any_value', 'any_hash')
+    expect(bcrypt.value).toBe('any_value')
+    expect(bcrypt.hash).toBe('any_hash')
   })
 })
