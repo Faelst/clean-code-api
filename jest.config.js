881b5f3e1fb1d4777466cd99dd27c38ff80065d8
Rafael Silverio <fael_st@hotmail.com>
2022-11-10 18:16:22 -0300

    remove files of jest coverage in main

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   clearMocks: true,
   coverageProvider: 'v8',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
   preset: '@shelf/jest-mongodb'
 }
