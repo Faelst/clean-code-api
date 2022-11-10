a15098aa9e683bfede03aa1437f190e1ad5ea2ab
Rafael Silverio <fael_st@hotmail.com>
2022-11-10 17:35:34 -0300

    move UpdateAccessTokenRepository to own file

---
+++ src/infra/repositories/update-access-token.js
@@ -0,0 +1,29 @@
+const {
+  MissingParamError
+} = require('../../utils/erros')
+
+class UpdateAccessTokenRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async update (userId, accessToken) {
+    if (!userId) {
+      throw new MissingParamError('userId')
+    }
+
+    if (!accessToken) {
+      throw new MissingParamError('accessToken')
+    }
+
+    await this.userModel.updateOne({
+      _id: userId
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+  }
+}
+
+module.exports = UpdateAccessTokenRepository
